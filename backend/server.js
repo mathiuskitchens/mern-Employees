f35c36862a5006158f9834d8c71cc46a7bfea343
mathiuskitchens <69939679+mathiuskitchens@users.noreply.github.com>
2023-04-22 12:27:56 -0400

    feat: Create express server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+const express = require("express");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+//express app
+const app = express();
+
+//middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//ROUTE handlers
+//respond to GET request
+app.get("/", (req, res) => {
+  res.json("message: received GET request");
+});
+
+//listening for requests on port
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
